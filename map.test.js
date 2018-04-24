de33cb8df2834da6eceda6b0a39aee8e82f2e846
Shelby Inloes <shelbyinloes@gmail.com>
2018-04-23 21:01:20 -0500

    new map test

--- map.test.js
+++ map.test.js
@@ -3,12 +3,12 @@ let dash = require("kkannen-dash");
 let map = dash.map
 
 const names = ["Jon","Bob","Ted","Barney","Lilly","Robin","Saul","Axe"];
-function onlyThreeLetters(name){
- return name.length === 3;
+function newHello(name){
+    return "Hello " + name;
 }
 
-let helloArray = filter(names,onlyThreeLetters);
+let helloArray = map(names,newHello);
 
-test('filter finds all names with 3 letters', () => {
- expect(helloArray).toEqual(["Jon","Bob","Ted","Axe"]);
+test('map adds hello before every name' , () => {
+ expect(helloArray).toEqual(["Hello Jon","Hello Bob","Hello Ted","Hello Barney","Hello Lilly","Hello Robin","Hello Saul","Hello Axe"]);
 });
